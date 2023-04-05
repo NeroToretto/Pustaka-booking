1776f2ae52da8dba888ddeae0dc96118464eaefb
NeroToretto <nerosanztuy@gmail.com>
2023-04-05 20:25:59 +0700

    P4

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -13,5 +13,6 @@ class Latihan1 extends CI_Controller
         $hasil = $this->Model_latihan1->jumlah($n1,$n2);
         echo "Hasil penjumlahan dari". $n1." + ". $n2 ." = "
     .$hasil;
+
     }
 }
